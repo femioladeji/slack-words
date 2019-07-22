480b5e10c96357c5c130283da8773f3e1d36957f
Femi Oladeji <f.oladeji@temper.works>
2019-07-23 01:17:48 +0200

    set up serverless framework

---
+++ utils/db.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+module.exports = {
+  insert(table, data) {
+    return new Promise((resolve, reject) => {
+      documentClient.put({
+        TableName: table,
+        Item: data
+      }, (error) => {
+        return error ? reject(error) : resolve(data)
+      });
+    });
+  }
+}
