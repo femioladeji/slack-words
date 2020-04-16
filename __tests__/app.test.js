4401fe72fc015f8d94fa684691ae62068ad980d1
Femi Oladeji <f.oladeji@temper.works>
2020-04-16 16:00:52 +0200

    fix test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -109,8 +109,8 @@ describe('computeResults', () => {
     user: 'user1',
     type: 'message',
   }, {
-    text: 'lej',
-    user: 'user1',
+    text: 'lje',
+    user: 'user2',
     type: 'invalid',
   }];
   const alphabetLetters = 'O M N M U M M L E J D T O X B'.toLowerCase().split(' ');
@@ -131,7 +131,7 @@ describe('computeResults', () => {
       },
       user2: {
         totalScore: 7,
-        words: 'mum: 3, ~lame~: 0, mole: 4',
+        words: 'mum: 3, ~lame~: 0, mole: 4, ~lje~: 0',
       },
     }, token);
   });
